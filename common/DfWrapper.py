4f2345da0acf9617c0f8f752625ae7d7fa3721ab
nex3z <litianxing9@gmail.com>
2018-07-15 14:55:27 +0800

    Update pandas based implementation.

---
+++ common/DfWrapper.py
@@ -0,0 +1,64 @@
+import pandas as pd
+
+
+class DfWrapper(object):
+    def __init__(self, values=None, name=''):
+        self.name = name
+        self.d = pd.DataFrame(columns=['prob'])
+        self.d.index.name = 'hypo'
+
+        if values is None:
+            return
+
+        if isinstance(values, dict):
+            self.__init_map(values)
+        else:
+            init_methods = [self.__init_sequence, self.__init_failure]
+            for method in init_methods:
+                try:
+                    method(values)
+                    break
+                except AttributeError:
+                    continue
+
+        if len(self.d) > 0:
+            self.normalize()
+
+    def __init_sequence(self, hypos):
+        for hypo in hypos:
+            self.set(hypo, 1.0)
+
+    def __init_map(self, mapping):
+        for hypo, prob in mapping.items():
+            self.set(hypo, prob)
+
+    def __init_failure(self, values):
+        raise ValueError('Initialization failed')
+
+    def hypos(self):
+        return self.d.index.values
+
+    def set(self, hypo, prob):
+        self.d.loc[hypo] = prob
+
+    def get(self, hypo, default=0):
+        return self.d.prob.get(hypo, default)
+
+    def mult(self, hypo, factor):
+        self.d.loc[hypo] = self.d.loc[hypo] * factor
+
+    def total(self):
+        total = self.d.prob.sum()
+        return 0 if not total else total
+
+    def normalize(self, fraction=1.0):
+        total = self.total()
+        if total <= 0:
+            raise ValueError('Total probability is zero.')
+
+        factor = float(fraction) / total
+        self.d.prob *= factor
+        return total
+
+    def print(self):
+        print(self.d)
--- common/Pmf.py
+++ common/Pmf.py
@@ -1,19 +1,9 @@
-from common.DictWrapper import DictWrapper
+from common.DfWrapper import DfWrapper
 
 
-class Pmf(DictWrapper):
-    def prob(self, key, default=0):
-        return self.d.get(key, default)
+class Pmf(DfWrapper):
+    def prob(self, event, default=0):
+        return super(Pmf, self).get(event, default)
 
-    def probs(self, keys):
-        return [self.prob(key) for key in keys]
-
-    def normalize(self, fraction=1.0):
-        total = self.total()
-        if total <= 0:
-            raise ValueError('Total probability is zero.')
-
-        factor = float(fraction) / total
-        for x in self.d.keys():
-            self.d[x] *= factor
-        return total
+    def probs(self, events):
+        return [self.prob(key) for key in events]
