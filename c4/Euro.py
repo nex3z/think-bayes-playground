6d1b941b1bf56b8b0faeb6cb514e345b69f09f0f
nex3z <litianxing9@gmail.com>
2018-07-28 21:56:18 +0800

    Add Euro.

--- c4/Euro.py
+++ c4/Euro.py
@@ -3,7 +3,7 @@ from common.Suite import Suite
 
 class Euro(Suite):
     def __init__(self, values=None):
-        super().__init__(values, hypo_desc='x')
+        super().__init__(values, value_desc='x')
 
     def likelihood(self, data, hypo):
         if data == 'H':
@@ -12,8 +12,9 @@ class Euro(Suite):
             return 1 - hypo / 100.0
 
     def summary(self):
+        cdf = self.make_cdf()
         print("Maximum likelihood: {}".format(self.max_likelihood()))
         print("Mean: {}".format(self.mean()))
-        print("Median: {}".format(self.percentile(50)))
-        print("Credible Interval: {}".format(self.credible_interval()))
+        print("Median: {}".format(cdf.percentile(50)))
+        print("Credible Interval: {}".format(cdf.credible_interval()))
         print("P(x = 50%) = {}".format(self.prob(50)))
